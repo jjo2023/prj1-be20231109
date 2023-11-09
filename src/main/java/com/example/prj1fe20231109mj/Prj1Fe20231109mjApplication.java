46e519d2975d7e455a9118c5d2e6543a314c9cfe
jjo2023 <snowman7685@gmail.com>
2023-11-09 11:41:19 +0900

    first commit

---
+++ src/main/java/com/example/prj1fe20231109mj/Prj1Fe20231109mjApplication.java
@@ -0,0 +1,13 @@
+package com.example.prj1fe20231109mj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj1Fe20231109mjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj1Fe20231109mjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prj1fe20231109mj/Prj1Fe20231109mjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prj1fe20231109mj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj1Fe20231109mjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
