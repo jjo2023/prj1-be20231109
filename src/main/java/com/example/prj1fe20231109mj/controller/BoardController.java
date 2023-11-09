9d1a5063e95a6beb905b94db9fa8af430c3101d0
jjo2023 <snowman7685@gmail.com>
2023-11-09 14:17:45 +0900

    빈 값이 있으면 badRequest 응답

--- src/main/java/com/example/prj1fe20231109mj/controller/BoardController.java
+++ src/main/java/com/example/prj1fe20231109mj/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.example.prj1fe20231109mj.controller;
 import com.example.prj1fe20231109mj.domain.Board;
 import com.example.prj1fe20231109mj.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,10 +12,32 @@ import org.springframework.web.bind.annotation.*;
 public class BoardController {
 
     private final BoardService service;
-    @PostMapping("add")
-    public void add(@RequestBody Board board){
 
-    service.save(board);
+    @PostMapping("add")
+    public ResponseEntity add(@RequestBody Board board) {
+
+        if (!service.validate(board))
+            return ResponseEntity.badRequest().build();
+        if (service.save(board)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.internalServerError().build();
+
+
+        }
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
