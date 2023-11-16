3ce5063c3a492b063ae2abcc283b13b0a8b06324
jjo2023 <snowman7685@gmail.com>
2023-11-16 10:43:22 +0900

    댓글 수정시 권한, 유효성 검사

--- src/main/java/com/example/prj1fe20231109mj/mapper/CommentMapper.java
+++ src/main/java/com/example/prj1fe20231109mj/mapper/CommentMapper.java
@@ -1,10 +1,7 @@
 package com.example.prj1fe20231109mj.mapper;
 
 import com.example.prj1fe20231109mj.domain.Comment;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -34,4 +31,11 @@ SELECT *FROM comment
 WHERE id=#{id}
 """)
     Comment selectById(Integer id);
+
+@Update("""
+UPDATE comment
+SET comment = #{comment}
+WHERE id = #{id}
+""")
+    int update(Comment comment);
 }
